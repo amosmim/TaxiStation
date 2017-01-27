ed78ad014d4c289d748fce09d2bd86c81feccd98
amosmim <amosmim@gmail.com>
2017-01-27 11:58:59 +0200

    some fixs for U2 and logging - ex5 submit files

--- Server.cpp
+++ Server.cpp
@@ -1,14 +1,21 @@
-//
-// Created by amos on 12/28/16.
-//
+/**
+ * Amos Maimon And Or Zipori.
+ *
+ */
 
 #include "Server.h"
+#include "easylogging++.h"
+
+_INITIALIZE_EASYLOGGINGPP
+
 int main(int argc,char *argv[]) {
 
     string input;
     string tamp;
     MainFlowClass mainFlow;
     int gridX, gridY;
+
+    LOG(INFO) << "Server started";
 
     // Get x and y for grid
     getline(cin, input);
@@ -42,5 +49,7 @@ int main(int argc,char *argv[]) {
     mainFlow.setGrid(gridX, gridY, obstaclesList);
     mainFlow.createTaxiStation(port, CONNECTION_TYPE);
     mainFlow.run();
+
+    LOG(INFO) << "Server ended.";
     return 0;
 }
