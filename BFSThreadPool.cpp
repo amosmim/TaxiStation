5f3e5586bcf44d6a6587613f289bbf972cb8773e
amosmim <amosmim@gmail.com>
2017-01-30 19:35:00 +0200

    Thread Pool with some fixs

--- BFSThreadPool.cpp
+++ BFSThreadPool.cpp
@@ -5,7 +5,7 @@
 
 
 #include "BFSThreadPool.h"
-#include "BFSThreadArgs.h"
+
 
 
 
@@ -13,49 +13,54 @@ BFSThreadPool::BFSThreadPool(int threadCount, Grid* map) {
     this->map = map;
     this->advance = true;
     this->threadCount = threadCount;
+    this->threads = new pthread_t[threadCount];
 
 }
 void BFSThreadPool::initialize(){
-    int failors = 0;
+
+    pthread_mutex_init(&lock, NULL);
     // create threads:
     for (int i=0; i < threadCount ; ++i) {
-        pthread_t pthread;
-        BFSThreadArgs args= BFSThreadArgs();
-        args.args = this;
-        int errorCheck = pthread_create(&pthread, NULL, startThread, (void*)&args);
+
+        //BFSThreadArgs args= BFSThreadArgs();
+        //args.args = this;
+        LOG(INFO) << "create BFS Thread no. " << i;
+        int errorCheck = pthread_create(threads + i, NULL, startThread, this);
 
         if(errorCheck != 0) {
             LOG(DEBUG) << "Failed to create Thread number  " << i << ".\n";
-            ++failors;
-        } else {
-            this->threads.push_back(pthread);
         }
 
     }
-    // make sure that not wait to thread that Does not exist.
-    this->threadCount -= failors;
+    LOG(DEBUG) << "finish initialize";
+
 }
 
 BFSThreadPool::~BFSThreadPool() {
     this->advance = false;
+
     for (int i = 0 ; i < threadCount; i++) {
         pthread_join(this->threads[i], NULL);
     }
+    delete[] threads;
+    pthread_mutex_destroy(&lock);
     LOG(INFO) << "BFSThreadPool finished!";
 }
 
 void *BFSThreadPool::executeThread() {
+    LOG(DEBUG) << "start executeThread()";
     TripInfo* tripInfo = NULL;
     BFS calculator = BFS();
-    pthread_mutex_t lock;
 
 
     while(this->advance){
-        while(this->tripInfoList.empty() && this->advance){sleep(1);}
+        //LOG(INFO) << "in while.";
+        //while(this->tripInfoList.empty() && this->advance){sleep(1);}
         pthread_mutex_lock(&lock);
         // make sure that the tripInfo doesn't gets from other thread.
-        if ((!this->tripInfoList.empty())&& this->advance) {
+        if ((!this->tripInfoList.empty()) && this->advance) {
             tripInfo = this->tripInfoList.front();
+            LOG (INFO) << "Thread pop trip no. " << tripInfo->getRideID();
             this->tripInfoList.pop_front();
             // give the other threads to get another trips before start the heavy markCalculated.
             pthread_mutex_unlock(&lock);
@@ -64,6 +69,9 @@ void *BFSThreadPool::executeThread() {
             // mark that the directions are calculate
             tripInfo->markCalculated();
             LOG(INFO) << "TripInfo number " << tripInfo->getRideID() << " is calculated!";
+        }else {
+            pthread_mutex_unlock(&lock);
+            sleep(1);
         }
 
     }
@@ -71,14 +79,17 @@ void *BFSThreadPool::executeThread() {
 }
 
 void BFSThreadPool::addTrip(TripInfo *tripInfo) {
+    LOG(DEBUG) << "tripinfo no. " << tripInfo->getRideID() << " add to Threads Poll";
     tripInfoList.push_back(tripInfo);
 }
 
-BFSThreadPool::BFSThreadPool() {}
 
 void *BFSThreadPool::startThread(void *self) {
-    BFSThreadArgs* bfsThreadArgs = (BFSThreadArgs*) self;
-    bfsThreadArgs->args->executeThread();
+    LOG(DEBUG)<< "in startThread()";
+    BFSThreadPool* pool = (BFSThreadPool*) self;
+    //BFSThreadArgs* bfsThreadArgs = (BFSThreadArgs*) self;
+    //bfsThreadArgs->args->executeThread();
+    pool->executeThread();
 
     return NULL;
 }
